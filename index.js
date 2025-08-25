3a6e1514e53d68e98209996835330aac1dab846d
Prakash Paudel <p.prakashpaudel35@gmail.com>
2025-08-25 19:55:11 +0545

    feat(apis): created the apis for the movie booking , get, post

---
+++ index.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const fs = require('fs');
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    fs.readFile("./index.html", "utf-8", (err, data) => {
+        if (err) {
+            return res.send({
+                status: 404,
+                error: err
+            })
+        }
+        return res.send(data)
+    })
+});
+
+// get all movies
+app.get("/movies", (req, res) => {
+    // res.send("GET all movies endpoint")
+    const db_value = fs.readFileSync("./db.json", "utf-8");
+    const parsedValue = JSON.parse(db_value);
+    res.send(
+        {
+            status: 200,
+            response: "success",
+            data: parsedValue["movies"]
+        }
+    )
+
+})
+
+// post movie
+app.post("/movie", (req, res) => {
+    try {
+        const data = fs.readFileSync("./db.json", "utf-8"); // here the data is been read
+        const parsedData = JSON.parse(data);                // once the data is been read then it is ben parsed
+
+        const movieList = parsedData.movies || [];          // once the movie is been parsed then the movies data is been get
+
+        const newMovie = req?.body
+
+        movieList.push(newMovie);
+
+        parsedData.movies = movieList;
+        fs.writeFileSync("./db.json", JSON.stringify(parsedData, null, 2), "utf-8");
+
+        res.json(movieList);
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: "Error updating db.json" });
+    }
+});
+
+app.listen(3000, () => {
+    console.log("started")
+})
